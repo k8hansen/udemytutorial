f897bbe96148a63d3a8573a9a3760b7566536f0a
Kate Hansen <k8hansen@gmail.com>
2021-01-26 15:21:05 -0500

    events

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
-import React, { Children } from 'react'
+// import React, { Children } from 'react'
 import ReactDom from 'react-dom';
 import './index.css';
 //setup vars
 
 const books =[
 {
+  id:1,
   img: 'https://upload.wikimedia.org/wikipedia/en/0/0c/MeTalkPrettyOneDayCover.JPG',
   title: 'Me Talk Pretty One Day',
   author: 'David Sedaris'
 },
 {
+  id:2,
   img: 'https://upload.wikimedia.org/wikipedia/en/c/c3/Marquis_de_Sade_-_The_120_Days_of_Sodom.jpeg',
   title: '100 Days of Sodom',
   author: 'Marquis de Sade'
+},
+{
+  id:3,
+  img: 'https://upload.wikimedia.org/wikipedia/commons/d/d2/Wizard_title_page.jpg',
+  title: 'Wizard of Oz',
+  author: 'Frank Baum'
 }
 ]
 
@@ -21,9 +29,8 @@ function BookList() {
   return ( 
   <section className="booklist">
     {books.map((book)=> {
-      const { img, title, author } = book;
       return (
-      <Book book={book}></Book>
+      <Book key={book.id} {...book}></Book>
         );
     })}
   </section>
@@ -31,11 +38,18 @@ function BookList() {
   }
 
 const Book = (props) => {
-  const { img, title, author } = props.book;
+//attribute, eventHandler
+//onClick, onMouseOver
+  const clickHandler = () => {
+    alert('Hello World')
+  }
+  const { img, title, author } = props;
   return (
-  <article className="book">
+  <article className="book" onMouseOver={()=>{
+    console.log(title)
+  }}>
     <img src={img} alt='' />
-    <h1>{title}</h1>
+    <h1 onClick={()=> console.log(title)}>{title}</h1>
     <h4>{author}</h4>
   </article>
   );
